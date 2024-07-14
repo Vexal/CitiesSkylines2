0a0ef9fed74560c7d3ae222bf2522a9133714f36
Vexal <porkchopsnbutter@gmail.com>
2024-07-14 15:32:55 -0500

    description

--- NoTrafficDespawn/Setting.cs
+++ NoTrafficDespawn/Setting.cs
@@ -44,14 +44,14 @@ namespace NoTrafficDespawn
 		{
 			return new Dictionary<string, string>
 			{
-				{ m_Setting.GetSettingsLocaleID(), "No Traffic Despawn" },
+				{ m_Setting.GetSettingsLocaleID(), "No Vehicle Despawn" },
 				{ m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
 
-				{ m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "No Traffic Despawn Settings" },
-				{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.generalInfo)), "This mod disables the automatic despawn of vehicles when a pathing deadlock is detected in the game. It applies to both cars and public transport vehicles. When this option is enabled, extra care must be taken to manually check for stuck traffic." },
+				{ m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "No Vehicle/Traffic Despawn Settings" },
+				{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.generalInfo)), "This mod disables the automatic despawn of vehicles when a pathing deadlock is detected in the game. It applies to both cars and public transport vehicles. When this option is enabled, extra care must be taken to manually check for stuck traffic. The effects of disabling/enabling despawning are immediate and can be toggled at any time." },
 
 				{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.trafficDespawnDisabled)), "Disable Traffic Despawn" },
-				{ m_Setting.GetOptionDescLocaleID(nameof(Setting.trafficDespawnDisabled)), $"Enable/disable vehicles from despawning when they are unable to make progress with pathing. Affects both road traffic and public transport." },
+				{ m_Setting.GetOptionDescLocaleID(nameof(Setting.trafficDespawnDisabled)), $"Disable vehicles from despawning when they are unable to make progress with pathing. Affects both road traffic and public transport." },
 
 
 			};
