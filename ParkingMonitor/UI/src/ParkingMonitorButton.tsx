d5d2323f36cdb5529825200fc01cc05f5f89a130
Vexal <porkchopsnbutter@gmail.com>
2024-11-12 01:25:33 -0600

    tables

--- ParkingMonitor/UI/src/ParkingMonitorButton.tsx
+++ ParkingMonitor/UI/src/ParkingMonitorButton.tsx
@@ -38,12 +38,14 @@ export default class ParkingMonitorButton extends Component {
 				this.setState({ hovering: true })
 			}} onMouseLeave={() => {
 				this.setState({ hovering: false })
-				}}>{ic}</div>
+				}}>
+				{ic}
+				</div>
 			{this.state.hovering && <ToolTip text={"Parking Monitor"} />}
 
-			{this.state.menuOpen && <div className={styles.cimCensusPanel}>
+			{this.state.menuOpen && <div className={styles.parkingMonitorPanel}>
 				<SectionHeader text="Parking Monitor" />
-				<div className={styles.cimCensusPanelBody}>
+				<div className={styles.parkingMonitorPanelBody}>
 					<PanelSection>
 						<div style={{ display: "flex" }}>
 							{ic}
@@ -95,10 +97,8 @@ export default class ParkingMonitorButton extends Component {
 						<span className={styles.parkingMonitorSectionText}>
 							<LocalizedText text={"Parking Monitor"} />
 						</span>
-						<div style={{maxHeight:"400rem", overflowY:"scroll"} }>
-							{this.state.dataBindings.map(data => <DataItem key={data.key} name={data.key} text={data.value.toString()} cb={(k: String) => {
-								
-							}} />)}
+						<div>
+							{this.state.dataBindings.map(data => <DataItem key={data.key} name={data.key} text={data.value.toString()} cb={(k: String) => {}} />)}
 						</div>
 						<div style={{marginTop:"10rem"} }>
 							<span className={styles.parkingMonitorSectionText }>
@@ -107,49 +107,31 @@ export default class ParkingMonitorButton extends Component {
 							<div className={styles.parkingMonitorLotList}>
 								{this.state.parkingBindings?.districtOrder.map((district, i) => {
 									const districtLots: any[] = this.state.parkingBindings?.districtMap[district] as any[];
-									console.log("it's rendering now");
 									return (
 										<div key={district}>
-											<div style={{ color:"rgba(55,195,241,1)"} }>
-												<span>{district} {districtLots.length} lots</span>
+											<div style={{ color: "rgba(55,195,241,1)", paddingTop:"10rem" }}>
+												<span>{district}</span>
 											</div>
-											<div style={{fontSize:"15rem"} }>
-												{districtLots.slice(0, Math.min(10, districtLots.length)).map((lot, lotInd) => {
-													return (
-														<div style={{ display: "flex" }} className={styles.dataItem} onClick={() => {
-															trigger(MOD_NAME, "selectLot", lot.key);
-														}}>
-															<span>
-																<span style={{ width: "20rem" }}>{lotInd + 1}</span> <b>{lot.name}</b> {lot.key}
-															</span>
-															<div style={{ flex: "1" }} />
-															<div>
-																{lot.count}
-															</div>
-														</div>
-													)
-                                                    /*
-													<span>
-																<span style={{width:"20rem"} }>{lotInd + 1}</span> <b>{lot.name}</b> {lot.key}
-															</span>
-															<div style={{ flex: "1" }} />
-															<div>
-																{lot.count}
-															</div>
-													return (
-                                                        <DataItem key={lotInd}
-                                                            index={lotInd + 1}
-                                                            rawText={lot.name}
-                                                            name={lot.key}
-                                                            text={lot.count.toString()}
-                                                            cb={(k: String) => {
-                                                                trigger(MOD_NAME, "selectLot", k);
-                                                            } } />);*/
-                                                })}
+											<div>
+												{districtLots.slice(0, Math.min(10, districtLots.length)).map((lot, lotInd) => <div style={{display:"flex", width:"420rem"}} key={lot.key }>
+													
+													<span style={{ width: "20rem" }} className={styles.tbl}>
+														{lotInd + 1}
+													</span>
+													<span style={{ width: "250rem", fontWeight:"bold" }} className={styles.tbl}>
+														{lot.name}
+													</span>
+													<span style={{ width: "130rem", fontSize:"15rem" }} className={styles.tbl}>
+														{lot.key}
+													</span>
+													<span style={{ width: "20rem" }} className={styles.tbl}>
+														{lot.count }
+													</span>
+												</div>)}
 											</div>
-										</div>
-									)
-                                })}
+										</div>)
+									
+								})}
 							</div>
 						</div>
 					</PanelSection>
@@ -258,7 +240,7 @@ export function DataItem(props: { name: string, rawText?: string | null, text: s
 
 function SectionHeader(props: { text: string }) {
 	const { translate } = useLocalization();
-	return <div className={styles.cimCensusPanelHeader} style={{ fontWeight: "bold" }}>{translate(MOD_NAME + props.text, props.text)}</div>
+	return <div className={styles.parkingMonitorPanelHeaderr} style={{ fontWeight: "bold" }}>{translate(MOD_NAME + props.text, props.text)}</div>
 }
 
 export function LocalizedText(props: { text: string }) {
@@ -276,3 +258,64 @@ const ic = <svg xmlns="http://www.w3.org/2000/svg"  width="34" height="34" viewB
 const playIcon = <svg fill="#FFFFFF" height="25rem" width="20rem" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30 c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15 C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"></path> <path d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30 S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"></path> </g> </g></svg>
 const pause = <svg fill="#FFFFFF" height="25rem" width="20rem" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M256,0C114.617,0,0,114.615,0,256s114.617,256,256,256s256-114.615,256-256S397.383,0,256,0z M224,320 c0,8.836-7.164,16-16,16h-32c-8.836,0-16-7.164-16-16V192c0-8.836,7.164-16,16-16h32c8.836,0,16,7.164,16,16V320z M352,320 c0,8.836-7.164,16-16,16h-32c-8.836,0-16-7.164-16-16V192c0-8.836,7.164-16,16-16h32c8.836,0,16,7.164,16,16V320z"></path> </g></svg>
 const stop = <svg fill="#FFFFFF" height="25rem" width="20rem" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 297 297" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M148.5,0C66.486,0,0,66.486,0,148.5S66.486,297,148.5,297S297,230.514,297,148.5S230.514,0,148.5,0z M213.292,190.121 c0,12.912-10.467,23.379-23.378,23.379H106.67c-12.911,0-23.378-10.467-23.378-23.379v-83.242c0-12.912,10.467-23.379,23.378-23.379 h83.244c12.911,0,23.378,10.467,23.378,23.379V190.121z"></path> </g></svg>
+
+
+/*
+<div key={district}>
+											<div style={{ color:"rgba(55,195,241,1)"} }>
+												<span>{district}</span>
+											</div>
+											<div style={{fontSize:"15rem"} }>
+												{districtLots.slice(0, Math.min(10, districtLots.length)).map((lot,lotInd) => {
+
+													/*return (
+														<div style={{ display: "flex" }} className={styles.dataItem} onClick={() => {
+															trigger(MOD_NAME, "selectLot", lot.key);
+														}}>
+															<div>
+																{lotInd}{lot.name}
+															</div>
+															<div style={{ flex: "1" }} />
+															<div>
+																{lot.count}
+															</div>
+														</div>
+													)*/
+/*
+<span>
+			<span style={{width:"20rem"} }>{lotInd + 1}</span> <b>{lot.name}</b> {lot.key}
+		</span>
+		<div style={{ flex: "1" }} />
+		<div>
+			{lot.count}
+		</div>
+return (
+	<DataItem key={lotInd}
+		index={lotInd + 1}
+		rawText={lot.name}
+		name={lot.key}
+		text={lot.count.toString()}
+		cb={(k: String) => {
+			trigger(MOD_NAME, "selectLot", k);
+		}} />);
+												})}
+											</div >
+										</div >
+
+
+
+
+
+										<table>
+											<tbody>
+												{districtLots.slice(0, Math.min(10, districtLots.length)).map((lot, lotInd) => <tr>
+													<td style={{ width: "20rem" }} className={styles.tbl}>
+														{lotInd + 1 }
+													</td>
+													<td style={{ width: "300rem" }} className={styles.tbl}>
+														{lot.name}
+													</td>
+												</tr>)}
+											</tbody>
+											</table>
+*/
